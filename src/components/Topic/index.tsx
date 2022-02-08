74aa25e279480116f1a19faa5ec70d1cdd914ccb
Dennis Montores <dennis.montores@outlook.com>
2022-02-08 17:01:10 -0600

    feat(topic): :sparkles: Component for showing the topic name and startgazers count

---
+++ src/components/Topic/index.tsx
@@ -0,0 +1,24 @@
+import type { TopicRelatedTopic } from '@app/services/queries/types'
+import { StargazerCounter } from '@app/components'
+import CSS from './Topic.module.css'
+
+interface TopicProps {
+  relTop: TopicRelatedTopic
+  handleClick: () => void
+}
+
+const Topic = ({ relTop, handleClick }: TopicProps): JSX.Element => {
+  return (
+    <div className={CSS.main} onClick={handleClick}>
+      <p className={CSS.topicName}>
+        <strong>Topic:</strong> {relTop.name}
+      </p>
+      <div style={{ width: '100%' }}>
+        <strong>Stars:</strong>&nbsp;
+        <StargazerCounter counter={relTop.stargazerCount} />
+      </div>
+    </div>
+  )
+}
+
+export default Topic
