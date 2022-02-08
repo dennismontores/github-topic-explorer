c69ff4f06a10835e09de4e02c64dfd2c89f45885
Dennis Montores <dennis.montores@outlook.com>
2022-02-08 17:06:11 -0600

    feat(root): :sparkles: Components integration
    Styling and layout added for displaying UI messages and the related topics

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import useSWR from 'swr'
+import { useSearchParams } from 'react-router-dom'
+import { Alert, SearchedTopic, TopicListing } from '@app/components'
+import { fetcher } from '@app/services'
+import { getTopics } from '@app/services/queries'
+import classes from './App.module.css'
+import type { TopicsResponse } from '@app/services/queries/types'
+import { useCallback, useEffect } from 'react'
 
-interface AppProps {}
+const App = () => {
+  const [search, setSearch] = useSearchParams()
+  const searchedTopic = search.get('search') || ''
+  const { data, error } = useSWR<TopicsResponse>([getTopics, { topic: searchedTopic }], fetcher)
 
-function App({}: AppProps) {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
+  useEffect(() => setSearch({ search: 'react' }), [])
+
+  const handleClick = useCallback((topicName: string) => setSearch({ search: topicName }), [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+    <section className={classes.main}>
+      <div className={classes.titleWrapper}>
+        <h1 className={classes.title}>Github Topic Explorer</h1>
+      </div>
+      <div className={classes.searchedTopicWrapper}>
+        <SearchedTopic searchedTopic={searchedTopic} />
+      </div>
+      {!data && <Alert type="info" message="Loading..." />}
+      {error && <Alert type="error" message="An error occurred while trying to retrieve the info!" />}
+      {data?.topic.relatedTopics.length === 0 && <Alert type="warning" message="No topics found!" />}
+      {data && data.topic.relatedTopics.length > 0 && (
+        <TopicListing topics={data.topic.relatedTopics} handleTopicClick={handleClick} />
+      )}
+    </section>
+  )
 }
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { StrictMode } from 'react'
+import { render } from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
   document.getElementById('root'),
-);
+)
 
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
 // Learn more: https://snowpack.dev/concepts/hot-module-replacement
 if (import.meta.hot) {
-  import.meta.hot.accept();
+  import.meta.hot.accept()
 }
