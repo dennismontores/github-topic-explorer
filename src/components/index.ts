24fcf16c98d1bd09f842f5cce9d98fd59bb0c73d
Dennis Montores <dennis.montores@outlook.com>
2022-02-08 17:04:50 -0600

    chore(components): :truck: A simple index file for exporting all the shared components

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as Alert } from './Alert'
+export { default as Topic } from './Topic'
+export { default as TopicListing } from './TopicListing'
+export { default as StargazerCounter } from './StargazerCounter'
+export { default as SearchedTopic } from './SearchedTopic'
