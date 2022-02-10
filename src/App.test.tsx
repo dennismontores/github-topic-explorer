cd6f7182cf7b2ad25abd381ec83c42ceda68426b
Dennis Montores <dennis.montores@outlook.com>
2022-02-10 12:52:03 -0600

    test(App component): :white_check_mark: Unit tests added to the main file - App

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,16 @@
-import * as React from 'react';
-import { render } from '@testing-library/react';
-import { expect } from 'chai';
-import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom'
+import { render } from '@testing-library/react'
+import { expect } from 'chai'
+import App from './App'
 
 describe('<App>', () => {
   it('renders learn react link', () => {
-    const { getByText } = render(<App />);
-    const linkElement = getByText(/learn react/i);
-    expect(document.body.contains(linkElement));
-  });
-});
+    const { getByText } = render(
+      <Router>
+        <App />
+      </Router>,
+    )
+    const linkElement = getByText(/Github topic explorer/i)
+    expect(document.body.contains(linkElement))
+  })
+})
